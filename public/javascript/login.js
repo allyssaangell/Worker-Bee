87ee1e1d9a0a37ed513b7356fbc5ad41a6599330
Caleb Luster <cwl117@outlook.com>
2022-04-04 19:23:35 -0500

    user not users

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -3,11 +3,12 @@ const loginFormHandler = async (event) => {
 
   // Collect values from the login form
   const email = document.querySelector("#email-login").value.trim();
+  console.log(email);
   const password = document.querySelector("#password-login").value.trim();
-
+  console.log(password);
   if (email && password) {
     // Send a POST request to the API endpoint
-    const response = await fetch("/api/users/login", {
+    const response = await fetch("/api/user/login", {
       method: "POST",
       body: JSON.stringify({ email, password }),
       headers: { "Content-Type": "application/json" },
@@ -30,7 +31,7 @@ const signupFormHandler = async (event) => {
   const password = document.querySelector("#password-signup").value.trim();
 
   if (name && email && password) {
-    const response = await fetch("/api/users", {
+    const response = await fetch("/api/user", {
       method: "POST",
       body: JSON.stringify({ name, email, password }),
       headers: { "Content-Type": "application/json" },
